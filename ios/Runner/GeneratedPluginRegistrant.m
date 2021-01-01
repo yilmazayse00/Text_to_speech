3ea28ae83e82dcbaf93f417f02f7eebdfc4f4298
Ayşe Nur Yılmaz <32266594+yilmazayse00@users.noreply.github.com>
2021-01-01 16:18:05 +0300

    Add files via upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_text_to_speech/FlutterTextToSpeechPlugin.h>)
+#import <flutter_text_to_speech/FlutterTextToSpeechPlugin.h>
+#else
+@import flutter_text_to_speech;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterTextToSpeechPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterTextToSpeechPlugin"]];
+}
+
+@end
